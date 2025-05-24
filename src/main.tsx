79a2d2e1997aa2d531fd773cd48e61a26db1b2f1
Harsh Nagrani <harshnagrani0910@gmail.com>
2025-05-24 16:02:23 +0530

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <Analytics />
   </QueryClientProvider>
 );
